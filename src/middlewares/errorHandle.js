f84a43c873e927a006d98d0d3dfb5ca6177e0df3
兵兵 <oncwnuAm0YChdC_sIUJPYk3qFJL0@git.weixin.qq.com>
2025-07-20 12:03:45 +0800

    优化错误处理中 Joi 验证错误的状态码逻辑

--- src/middlewares/errorHandle.js
+++ src/middlewares/errorHandle.js
@@ -4,8 +4,8 @@ import { ERROR_TYPES } from '../consts.js'; // 假设你有一个 errorTypes.js 
 export default (err, req, res, next) => {
   console.log('错误处理自定义中间件：', err, err?.isJoi);
   let statusCode = 500;
+  // 专门处理 Joi 验证错误
   if (err.isJoi || err instanceof Joi.ValidationError) {
-    // statusCode = 400;
     // 针对未知字段错误返回 422
     if (err.details && err.details.some(detail => detail.type === 'object.unknown')) {
       statusCode = 422;
